91aa2f01212f73ac694dcba9f09ed7641a597165
Jessica Garrido <jessica.garrido.di@gmail.com>
2023-06-19 22:01:53 +0200

    affichage dynamique de chaque pokémon sur le bloc de gauche

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,23 @@
 import React from "react";
-import ImgPikachu from "../pika.png";
 
-export default function Card() {
+export default function Card({ pokemon, loading, infopokemon }) {
   return (
-    <div>
-      <div>
-        <div className="pokeList">
-          <h4>Id : 1</h4>
-          <img src={ImgPikachu} alt="image de test" />
-          <span>Pikachu</span>
-        </div>
-      </div>
+    <div style={{ display: "flex", flexWrap: "wrap", justifyContent:"center", gap:"1em" }}>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        pokemon.map((item) => {
+          return (
+            <div key={item.id} onClick={() => infopokemon(item)}>
+              <div className="pokeList">
+                <h4>{item.id}</h4>
+                <img src={item.sprites.front_default} alt={item.name} />
+                <span>{item.name}</span>
+              </div>
+            </div>
+          );
+        })
+      )}
     </div>
   );
 }
