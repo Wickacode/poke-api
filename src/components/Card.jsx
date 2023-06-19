14b4f12a280a8231e8204395346cd78558b52a55
Jessica Garrido <jessica.garrido.di@gmail.com>
2023-06-19 21:31:30 +0200

    mise en place de la méthode fetch (j'ai d'abord testé avec axios mais j'ai changé d'avis)

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import ImgPikachu from "../pika.png"
+import React from "react";
+import ImgPikachu from "../pika.png";
 
 export default function Card() {
   return (
     <div>
-      <div className="pokeList">
-        <h4>Id : 1</h4>
-        <img src={ImgPikachu} alt="image de test" />
-        <span>Pikachu</span>
+      <div>
+        <div className="pokeList">
+          <h4>Id : 1</h4>
+          <img src={ImgPikachu} alt="image de test" />
+          <span>Pikachu</span>
+        </div>
       </div>
     </div>
-  )
+  );
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,19 +1,50 @@
-import React from "react"
-import Card from "./Card"
-import CardInfo from "./CardInfo"
+import React, { useState, useEffect } from "react";
+import Card from "./Card";
+import CardInfo from "./CardInfo";
 
 export default function Main() {
-    return(
-        <div className="container">
-            <div className="leftSpace">
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            </div>
-            <div className="rightSpace">
-            <CardInfo />
-            </div>
-        </div>
-    )
+
+  // Différents états du composant 
+  const [pokeData, setPokeData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [url, setUrl] = useState("https://pokeapi.co/api/v2/pokemon/");
+  const [pokeDex, setPokeDex] = useState();
+
+  //Récupération des données depuis l'API
+  const pokeFunction = async () => {
+    setLoading(true);
+    const res = await fetch(url);
+    const data = await res.json();
+    getPokemon(data.results);
+    setLoading(false);
+  };
+
+  //Récupération des inforamtions détaillées d'un Pokémon
+  const getPokemon = async (results) => {
+    results.map(async (item) => {
+      const response = await fetch(item.url);
+      const data = await response.json();
+      setPokeData((state) => {
+        state = [...state, data];
+        state.sort((a, b) => (a.id > b.id ? 1 : -1));
+        return state;
+        
+      });
+    });
+  };
+
+  useEffect(() => {
+    pokeFunction();
+  }, [url]);
+
+  return (
+    <div className="container">
+      <div className="leftSpace">
+        <Card pokemon={pokeData} loading={loading} infopokemon={(poke) => setPokeDex(poke)} />
+      </div>
+      <div className="rightSpace">
+        <CardInfo data={pokeDex} />
+      </div>
+    </div>
+  );
 }
